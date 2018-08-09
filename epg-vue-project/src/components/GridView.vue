8d96fdb61e3c408f217067709ec8a98832c003e2
gwenananaelle <gwenaelle.duchesne@gmail.com>
2018-08-09 17:03:50 +0200

    oups

--- epg-vue-project/src/components/GridView.vue
+++ epg-vue-project/src/components/GridView.vue
@@ -13,41 +13,11 @@ import { eventState } from '../states/event-state'
 export default {
   name: 'GridView',
   components: {
-<<<<<<< HEAD
-    Row
-  },
-  async created () {
-    try {
-      this.loading = true
-      const response = await fetch('data/GenericCategories.json')
-      this.movies = await response.json()
-      let list = this.movies.map(element => {
-        return { content_nibble_lvl_1: element.content_nibble_lvl_1 }
-      })
-      list.sort()
-      const reduce = list.reduce((categories, movie) => {
-        !categories.includes(movie.content_nibble_lvl_1) && categories.push(movie.content_nibble_lvl_1)
-        return categories
-      }, [])
-      console.log(reduce)
-    } catch (error) {
-      console.log(error)
-    } finally {
-      this.loading = false
-    }
-  },
-  data () {
-    return {
-      movies: null,
-      loading: false
-    }
-=======
     Row,
     ProgramDetail
   },
   data () {
     return { eventState }
->>>>>>> 00cb5e38738aa5d34e754e5831f0ad13640ef53b
   }
 }
 </script>
