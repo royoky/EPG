f9798751c330053c4eb39d0d8deab4afbe3ee26a
groy <41110605+gwennaelle@users.noreply.github.com>
2018-08-20 16:40:33 +0200

    add programms

--- epg-vue-project/src/components/GridView.vue
+++ epg-vue-project/src/components/GridView.vue
@@ -1,18 +1,40 @@
 <template>
     <main>
-        <Row v-for="n in 10"
-        :key="n.id"/>         
+        <Card  v-for="(event, index ) in events" 
+          :key="index" 
+          :event="event" 
+          :selectEvent="selectEvent"/>         
     </main>
 </template>
 
 <script>
-import Row from './Row.vue'
+import Card from './Card.vue'
 
 export default {
   name: 'GridView',
   components: {
-    Row
-  }
+    Card
+  }, 
+  data () { 
+    return { 
+      events: null, 
+      selectedEvent: null 
+    } 
+  }, 
+  async created () { 
+    try { 
+      let response = await fetch('data/GenericEvents.json') 
+      this.events = await response.json() 
+      console.log(this.events)
+    } catch (error) { 
+      console.error(error) 
+    } 
+  }, 
+  methods: { 
+    selectEvent (event) { 
+      this.selectedEvent = event 
+    } 
+  } 
 }
 </script>
 
@@ -20,6 +42,11 @@ export default {
 <style scoped lang="less">
 main {
   background-color: white;
+  flex-grow: 1;
+  overflow: scroll;
   display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: flex-start;
 }
 </style>
