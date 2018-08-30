959981fc8ad0f37cd1603cba6e250a33ab69202e
gwenananaelle <gwenaelle.duchesne@gmail.com>
2018-08-30 17:04:02 +0200

    test merge

--- epg-vue-project/src/components/NavigationBar.vue
+++ epg-vue-project/src/components/NavigationBar.vue
@@ -3,7 +3,11 @@
         <ul>
             <li tabindex="-1" @click="getEventNow()"> now</li>
             <li tabindex="-1" @click="getEventTonight()">tonight</li>
+<<<<<<< HEAD
             <li tabindex="-1" @click="getEventByCat(10)">category</li>
+=======
+            <li tabindex="-1">category</li>
+>>>>>>> now is working and tonight also
             <li tabindex="-1">channel</li>
         </ul>
         <ul v-if="this.navigationState.selectedCategory">
@@ -27,15 +31,27 @@ export default {
       this.navigationState.selectedCategory = null     
       const events = await fetch('data/GenericEvents.json')
       let listOfEvents = await events.json()
+<<<<<<< HEAD
       const endNow = moment(this.navigationState.today, 'X').add(1, 'h').format('X')
       listOfEvents = listOfEvents.filter(element => element.start_date > this.navigationState.today && element.start_date < this.navigationState.today + 86400)
       listOfEvents = listOfEvents.filter(element => element.start_date < endNow && element.end_date > this.navigationState.today)
+=======
+      const now = new Date().getHours()
+      function compareHours (timeStamp) {
+        let hours = new Date(timeStamp * 1000).getHours()
+        return hours === now
+      }
+      listOfEvents = listOfEvents.filter(element => compareHours(element.start_date))
+>>>>>>> now is working and tonight also
       this.navigationState.programList = listOfEvents
     },
     async getEventTonight () {
       const events = await fetch('data/GenericEvents.json')
       let listOfEvents = await events.json()
+<<<<<<< HEAD
       listOfEvents = listOfEvents.filter(element => element.start_date > this.navigationState.today && element.start_date < this.navigationState.today + 86400)
+=======
+>>>>>>> now is working and tonight also
       listOfEvents = listOfEvents.filter(element => new Date(element.start_date * 1000).getHours() <= 23 && new Date(element.start_date * 1000).getHours() >= 20)
       this.navigationState.programList = listOfEvents
     },
