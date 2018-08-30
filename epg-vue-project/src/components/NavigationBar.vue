54591cec5a63738eaeb5d5eb199f84c920296d8c
gwenananaelle <gwenaelle.duchesne@gmail.com>
2018-08-30 16:08:48 +0200

    add moment

--- epg-vue-project/src/components/NavigationBar.vue
+++ epg-vue-project/src/components/NavigationBar.vue
@@ -3,14 +3,14 @@
         <ul>
             <li tabindex="-1" @click="getEventNow()"> now</li>
             <li tabindex="-1" @click="getEventTonight()">tonight</li>
-            <li tabindex="-1">category</li>
+            <li tabindex="-1" @click="getEventByCat(10)">category</li>
             <li tabindex="-1">channel</li>
         </ul>
-        <ul>
-            <li @click="getEventByCat(100)">100</li>
-            <li @click="getEventByCat(10 )">10</li>
-            <li @click="getEventByCat(50 )">50</li>
-            <li @click="getEventByCat(60 )">60</li>
+        <ul v-if="this.navigationState.selectedCategory">
+            <li @click="getEventByCat(100)">émissions</li>
+            <li @click="getEventByCat(10 )">films / séries</li>
+            <li @click="getEventByCat(50 )">jeunesse</li>
+            <li @click="getEventByCat(60 )">spectacle</li>
             <li @click="getEventByCat(90 )">documentaire</li>
             <li @click="getEventByCat(40)">sport</li>
         </ul>
@@ -19,23 +19,23 @@
 
 <script>
 import { navigationState } from '../states/navigation-state'
+import moment from 'moment'
 export default {
   name: 'navigationBar',
   methods: {
-    async getEventNow () {
+    async getEventNow () { 
+      this.navigationState.selectedCategory = null     
       const events = await fetch('data/GenericEvents.json')
       let listOfEvents = await events.json()
-      const now = new Date().getHours()
-      function compareHours (timeStamp) {
-        let hours = new Date(timeStamp * 1000).getHours()
-        return hours === now
-      }
-      listOfEvents = listOfEvents.filter(element => compareHours(element.start_date))
+      const endNow = moment(this.navigationState.today, 'X').add(1, 'h').format('X')
+      listOfEvents = listOfEvents.filter(element => element.start_date > this.navigationState.today && element.start_date < this.navigationState.today + 86400)
+      listOfEvents = listOfEvents.filter(element => element.start_date < endNow && element.end_date > this.navigationState.today)
       this.navigationState.programList = listOfEvents
     },
     async getEventTonight () {
       const events = await fetch('data/GenericEvents.json')
       let listOfEvents = await events.json()
+      listOfEvents = listOfEvents.filter(element => element.start_date > this.navigationState.today && element.start_date < this.navigationState.today + 86400)
       listOfEvents = listOfEvents.filter(element => new Date(element.start_date * 1000).getHours() <= 23 && new Date(element.start_date * 1000).getHours() >= 20)
       this.navigationState.programList = listOfEvents
     },
@@ -81,7 +81,6 @@ ul {
     overflow: hidden;
     background-color: #333;
 }
-
 li {
     float: left;
     display: block;
