54ad0e5f7a22ce41a34dd35ca5eea00199aefd8a
groy <41110605+gwennaelle@users.noreply.github.com>
2018-09-06 14:32:47 +0200

    minicorrrectif

--- epg-vue-project/src/states/navigation-state.js
+++ epg-vue-project/src/states/navigation-state.js
@@ -3,8 +3,8 @@ export const navigationState = {
   today: moment('11-26-2013', 'MM-DD-YYYY').set('h', moment().get('h')).set('m', moment().get('m')).format('X'),
   categoryList: [],
   programList: [],
-  programAll: []
+  programAll: [],
   programList: null,
-  catNavbar: false
+  catNavbar: false,
   selectedCategory: null,
 }
